91a8245f7c20203e48ee8cf0a0fd44ccda101797
Adarsh S Kumar <adarshskumar@Adarshs-Air.bbrouter>
2023-04-29 11:21:59 +0530

    Updates

---
+++ precache-manifest.4c79c142e42bf92593473702b631a171.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "2f4d146aba8f9395a9b1",
+    "url": "/Olx_Clone-React/static/css/main.a74882b2.chunk.css"
+  },
+  {
+    "revision": "2f4d146aba8f9395a9b1",
+    "url": "/Olx_Clone-React/static/js/main.2f4d146a.chunk.js"
+  },
+  {
+    "revision": "4841cb22b64e3b6ba8b5",
+    "url": "/Olx_Clone-React/static/js/1.4841cb22.chunk.js"
+  },
+  {
+    "revision": "577afa3c1b76030c0034",
+    "url": "/Olx_Clone-React/static/js/runtime~main.577afa3c.js"
+  },
+  {
+    "revision": "1202463180d7c17ec35b9095f643efe8",
+    "url": "/Olx_Clone-React/static/media/olx-logo.12024631.png"
+  },
+  {
+    "revision": "ce0b833cffdf42224be1643d186a88f8",
+    "url": "/Olx_Clone-React/index.html"
+  }
+];
